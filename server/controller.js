a2b0f9d725e2c5c406ac3df111031d8352f0d17c
Kaden Klein <kadenklein@MacBook-Pro.local>
2020-07-06 16:11:27 -0800

    Fixed images not rendering

--- server/controller.js
+++ server/controller.js
@@ -15,8 +15,8 @@ module.exports = {
     },
     addItem: (req, res) => {
         const db = req.app.get('db');
-        const {name, price, imgurl} = req.body;
-        db.create_product(name, price, imgurl)
+        const {name, price, img} = req.body;
+        db.create_product(name, price, img)
         .then(() => res.sendStatus(200))
         .catch(err => res.status(500).send(err))
     },
